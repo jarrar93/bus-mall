661f88f99cd5fdb4c0ad06a819988e4cfe18a113
jarrar93 <mohammadjarrar810@yahoo.com>
2021-05-22 01:48:51 -0700

    masterbus

--- js/app.js
+++ js/app.js
@@ -1,49 +1,249 @@
-"use strict";
-let images = ['1.png', 'bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
-let productArray = [];
-let productToView = [];
+'use strict';
+//Global
+var productImg = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg']
+var leftProImage = document.querySelector('#pro1img');
+var rightProImage = document.querySelector('#pro2img');
+var centerProImage = document.querySelector('#pro3img');
+var groupImageSection = document.getElementById('all_Pro');
+var product = [];
+var noRepeat = [];
+var totalClicks = 1; // to stop the prossess after 25 click
 
-
-function Myproduct(name, imageUrl, count) {
-
-  this.name = name,
-    this.imageUrl = imageUrl;
-  this.count = count;
-
-}
-for (let i = 0; i < images.length; i++) {
-
-  var nameOfpro = images[i].split(".")[0];
-
-  let pordcut1 = new Myproduct(nameOfpro, images[i], 0)
-
-  productArray.push(pordcut1);
-
+function Products(name) {
+  this.name = name;
+  this.urlImg = `img/${this.name}`;
+  this.totalClickspro = 0;
+  this.totalCountViews = 0;
+  product.push(this);//this its refer to the object that im created
 }
 
-function genrateProduct() {
-  for (let i = 0; i < 3; i++) {
+//Random image:----------->
+function randomImg() {
+  var leftProRandom = product[randomNumber(0, product.length - 1)]
+  var rightPro2Random = product[randomNumber(0, product.length - 1)]
+  var centerPro3Random = product[randomNumber(0, product.length - 1)]
+  leftProImage.setAttribute('src', leftProRandom.urlImg);
+  leftProImage.setAttribute('alt', leftProRandom.name);
+  rightProImage.setAttribute('src', rightPro2Random.urlImg);
+  rightProImage.setAttribute('alt', rightPro2Random.name);
+  centerProImage.setAttribute('src', centerPro3Random.urlImg);
+  centerProImage.setAttribute('alt', centerPro3Random.name);
+  while ((leftProRandom.name === rightPro2Random.name) ||
+    (leftProRandom.name === centerPro3Random.name) ||
+    (centerPro3Random.name === rightPro2Random.name) ||
+    (noRepeat.includes(leftProRandom.name)) ||
+    (noRepeat.includes(rightPro2Random.name)) ||
+    (noRepeat.includes(centerPro3Random.name))) {
+    //pick another random number
+    var leftProRandom = product[randomNumber(0, product.length - 1)]
+    var rightPro2Random = product[randomNumber(0, product.length - 1)]
+    var centerPro3Random = product[randomNumber(0, product.length - 1)]
+    leftProImage.setAttribute('src', leftProRandom.urlImg);
+    leftProImage.setAttribute('alt', leftProRandom.name);
+    rightProImage.setAttribute('src', rightPro2Random.urlImg);
+    rightProImage.setAttribute('alt', rightPro2Random.name);
+    centerProImage.setAttribute('src', centerPro3Random.urlImg);
+    centerProImage.setAttribute('alt', centerPro3Random.name);
+  }
+  noRepeat = [];
+  noRepeat.push(leftProRandom.name, rightPro2Random.name, centerPro3Random.name);
+  leftProRandom.totalCountViews += 1;
+  rightPro2Random.totalCountViews += 1;
+  centerPro3Random.totalCountViews += 1;
+  setItem()
+}
 
-    let randomNum = Math.floor(Math.random() * images.length);
-    productToView.push(productArray[randomNum])
+for (var i = 0; i < productImg.length; i++) {
+  new Products(productImg[i]);
+}
 
+getItem();
+randomImg();
+
+function clickImage(e) {
+  totalClicks = totalClicks + 1;
+
+  for (var i = 0; i < product.length; i++) {
+    if (product[i].name === e.target.alt) {
+      product[i].totalClickspro += 1;
+    }
   }
 
-  console.log(productToView);
-  appendProduct()
+  if (totalClicks == 26) {
+    groupImageSection.removeEventListener('click', clickImage)
+    leftProImage.remove()
+    rightProImage.remove()
+    centerProImage.remove()
+    console.log('finished');
+    renderFinsh()
+    alert('finished')
+    renderChartResults()
+    return;
+  };
+
+  if ((e.target.id === 'pro1img') || (e.target.id === 'pro2img') || (e.target.id === 'pro3img')) {
+    randomImg();
+  }
 }
-genrateProduct();
 
-function appendProduct() {
+groupImageSection.addEventListener('click', clickImage);
 
-  let showImage1 = document.getElementById('product1');
-  let showImage2 = document.getElementById('product2');
-  let showImage3 = document.getElementById('product3');
-  let firstSourc = './img/' + productToView[0].imageUrl;
-  let secSourc = './img/' + productToView[1].imageUrl;
-  let thdSourc = './img/' + productToView[2].imageUrl;
-  showImage1.src = firstSourc;
-  showImage2.src = secSourc;
-  showImage3.src = thdSourc;
-
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function renderFinsh() {
+  var list = document.getElementById("list");
+  var ulel = document.createElement('ul');
+  list.appendChild(ulel);
+  for (var i = 0; i < product.length; i++) {
+    var liel = document.createElement('li');
+    liel.textContent = `${product[i].name} had ${product[i].totalClickspro} votes and was shown ${product[i].totalCountViews} times`
+    ulel.appendChild(liel)
+  }
+}
+
+
+function renderChartResults() {
+  var proNames = [];
+  var proClicks = [];
+  var proViews = [];
+  for (var i = 0; i < product.length; i++) {
+    var pronames = product[i].name;
+    proNames.push(pronames);
+    var pronlicks = product[i].totalClickspro;
+    proClicks.push(pronlicks);
+    var proviews = product[i].totalCountViews;
+    proViews.push(proviews);
+  }
+  console.log(proNames)
+  console.log(proClicks)
+  console.log(proViews)
+  var ctx = document.getElementById('myChart').getContext('2d');
+  //chart render:----------->
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: proNames,
+      datasets: [{
+        label: '# of Votes',
+        data: proClicks,
+        backgroundColor: [
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)'
+        ],
+        borderColor: [
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(255, 206, 86, 0.7)'
+        ],
+      }, {
+        label: '# of Views',
+        data: proViews,
+        backgroundColor: [
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)'
+
+        ],
+        borderColor: [
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(153, 102, 255, 0.7)'
+
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+}
+
+//update drinks
+function setItem() {
+  var SetPro = JSON.stringify(product);
+  localStorage.setItem('proDuct1', SetPro);
+}
+
+//get all drinks
+function getItem() {
+  var proDuct1 = localStorage.getItem('proDuct1');
+  if (JSON.parse(proDuct1) != null) {
+    product = JSON.parse(proDuct1)
+  }
+}
+// Add an event listener to the submit button
+groupImageSection.addEventListener('click', clickImage);
